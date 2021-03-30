1506448d440d55add5e5ecd8f449718f47a79687
Nerea García <nereagarcia.c@gmail.com>
2021-03-30 19:53:37 +0200

    Exportar función guardarSaludo

--- utilidades/archivos.js
+++ utilidades/archivos.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 const nombre = "";
 
 const guardarSaludo = (nombre, () => {
   const saludo = (`hola${nombre}. Encantando de saludarte.`);
-  module.exports = { saludo };
 });
+
+module.exports = guardarSaludo;
